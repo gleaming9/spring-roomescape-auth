f19b658b2e03af85faf010c98c1de6d0ed0838d7
gleaming9 <gleaming9@khu.ac.kr>
2026-05-19 17:37:05 +0900

    feat: 회원 도메인 도입 및 예약과 회원 연결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
 CREATE TABLE theme
 (
     id          BIGINT        NOT NULL AUTO_INCREMENT,
@@ -18,14 +28,15 @@ CREATE TABLE reservation_time
 
 CREATE TABLE reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    date     DATE         NOT NULL,
-    time_id  BIGINT       NOT NULL,
-    theme_id BIGINT       NOT NULL,
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    date      DATE         NOT NULL,
+    time_id   BIGINT       NOT NULL,
+    theme_id  BIGINT       NOT NULL,
+    member_id BIGINT       NOT NULL,
 
     PRIMARY KEY (id),
     UNIQUE (date, theme_id, time_id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
